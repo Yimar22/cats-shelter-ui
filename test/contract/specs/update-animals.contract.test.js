ec8a1fa4e7f800ce3750cbcab36c41d21768cd05
Yimar T <42156268+Yimar22@users.noreply.github.com>
2023-05-21 22:14:44 -0500

    fixed animalcontroller (#17)

--- test/contract/specs/update-animals.contract.test.js
+++ test/contract/specs/update-animals.contract.test.js
@@ -1,18 +1,19 @@
 import { provider } from '../config/init-pact.js';
-import { Matchers } from '@pact-foundation/pact';
+import {Matchers} from '@pact-foundation/pact';
 import { AnimalController } from '../../../controllers/AnimalsController.js';
 import { expect } from 'chai';
+import {HttpStatusCode} from "axios";
 
-describe('Animal Service - Update animal', () => {
-    describe('When a request to update a cat is made', () => {
+describe('Animal Service', () => {
+    describe('When a request to update an animal is made', () => {
         before(async () => {
             await provider.setup();
             await provider.addInteraction({
-                uponReceiving: 'a request to update a cat',
-                state: "has animal to update",
+                uponReceiving: 'a request to update an animal',
+                state: "there is one animal to update",
                 withRequest: {
                     method: 'PUT',
-                    path: Matchers.string('/animals/{name}'),
+                    path: '/animals/Manchas',
                     body: Matchers.somethingLike({
                         name: Matchers.like('Manchas'),
                         breed: Matchers.like("Bengali"),
@@ -45,10 +46,7 @@ describe('Animal Service - Update animal', () => {
             const response = await AnimalController.updateAnimal(manchasCat.name, manchasCat);
             const responseBody = response.data;
 
-            // Verifying response is not undefined
             expect(responseBody).to.not.be.undefined;
-
-            // Verifying data properties within response
             expect(responseBody).to.have.property('name');
             expect(responseBody).to.have.property('breed');
             expect(responseBody).to.have.property('gender');
@@ -57,6 +55,7 @@ describe('Animal Service - Update animal', () => {
             // Verifying response data is equal to expected data
             expect(responseBody).to.be.eql(manchasCat);
 
+            expect(response.status).to.be.equal(HttpStatusCode.Ok);
             await provider.verify()
         });
     });
