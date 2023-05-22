ec066e7b45f8110716d81775e591a5e19f409eb9
Yimar T <42156268+Yimar22@users.noreply.github.com>
2023-05-21 21:55:12 -0500

    Implemented test contract of deleteAnimal (#16)

---
+++ test/contract/specs/delete-animals.contract.test.js
@@ -0,0 +1,34 @@
+import { provider } from '../config/init-pact.js';
+import { Matchers } from '@pact-foundation/pact';
+import { AnimalController } from '../../../controllers/AnimalsController.js';
+import { expect } from 'chai';
+
+describe('Animal Service - Delete animal', () => {
+    describe('When a request to delete a cat is made', () => {
+        before(async () => {
+            await provider.setup();
+            await provider.addInteraction({
+                uponReceiving: 'a request to delete a cat',
+                state: "has animals to delete",
+                withRequest: {
+                    method: 'DELETE',
+                    path: Matchers.string('/animals/{name}')
+                },
+                willRespondWith: {
+                    status: 204
+                }
+            });
+        });
+
+        after(() => provider.finalize());
+
+        it('should return the correct data', async () => {
+            const response = await AnimalController.delete('Manchas');
+
+            // Verifying status within response
+            expect(response.status).to.be.eql(204);
+
+            await provider.verify()
+        });
+    });
+});
